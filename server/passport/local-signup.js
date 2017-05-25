e867b3dd30af5ff647c5cf5d2004de315f8adb13
dnajafi <dbnajafi@gmail.com>
2017-05-24 17:42:55 -0700

    finish auth routes for both login and signup

---
+++ server/passport/local-signup.js
@@ -0,0 +1,26 @@
+const User = require('mongoose').model('User');
+const PassportLocalStrategy = require('passport-local').Strategy;
+
+
+/**
+ * Return the Passport Local Strategy object.
+ */
+module.exports = new PassportLocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  session: false, //since we are using token for authentication
+  passReqToCallback: true //required to be true if we want to be able to read other parameters in the POST body message.
+}, (req, email, password, done) => {
+  const userData = {
+    email: email.trim(),
+    password: password.trim(),
+    name: req.body.name.trim()
+  };
+
+  const newUser = new User(userData);
+  newUser.save((err) => {
+    if (err) { return done(err); }
+
+    return done(null);
+  });
+});
